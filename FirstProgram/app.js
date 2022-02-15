579473c8b6d5991c87200e218153c9b457a73b2d
Ahmed Tahir <ahmed.tahir@optimusfox.com>
2022-02-15 18:03:42 +0500

    firstProgram

---
+++ FirstProgram/app.js
@@ -0,0 +1,23 @@
+function a(name) {
+  console.log("hello " + name);
+  console.log(this.global);
+}
+
+a("ahmed");
+
+//Window is not in Node .... it has global
+
+// <ref *1> Object [global] {
+//   global: [Circular *1],
+//   clearInterval: [Function: clearInterval],
+//   clearTimeout: [Function: clearTimeout],
+//   setInterval: [Function: setInterval],
+//   setTimeout: [Function: setTimeout] {
+//     [Symbol(nodejs.util.promisify.custom)]: [Getter]
+//   },
+//   queueMicrotask: [Function: queueMicrotask],
+//   clearImmediate: [Function: clearImmediate],
+//   setImmediate: [Function: setImmediate] {
+//     [Symbol(nodejs.util.promisify.custom)]: [Getter]
+//   }
+// }
