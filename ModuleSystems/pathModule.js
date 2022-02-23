93a82eadd1c3a09d3c043d42cd14fafa9419b0da
Ahmed Tahir <ahmed.tahir@optimusfox.com>
2022-02-23 15:47:17 +0500

    Path Base Name

--- ModuleSystems/pathModule.js
+++ ModuleSystems/pathModule.js
@@ -1,7 +1,7 @@
 const path = require("path");
 // console.log(path);
 
-// =================================PATH BASENAEM
+// =================================PATH BASNAME
 // console.log(`${path.basename("C:\\temp\\myfile.html")}`);
 // getting the base of any othfile path in any OS other than Window
 const base = path.basename("/foo/bar/baz/asdf/quux.html");
@@ -14,3 +14,5 @@ const baseWin32 = path.win32.basename("/foo/bar/baz/asdf/quux.html");
 // console.log(baseWin32)
 
 // A TypeError is thrown if path is not a string or if ext is given and is not a string.
+
+// ===============================
